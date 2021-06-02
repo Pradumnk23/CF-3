e29cae71f9045ff6cec7bd57f1778b8d72356992
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-06-02 22:21:33 +0530

    if(i==j || i==j+1 || (i==0 && j==n-1))-> p("1") ;

---
+++ Prime Square.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define mod                 1000000007
+const int N=2e5+100;
+
+int main()
+{
+    fastio;
+    test()
+    {
+        lli n;
+        cin>>n;
+        fn
+        {
+            for(lli j=0; j<n; j++)
+            {
+                if(i==j || i==j+1 || (i==0 && j==n-1))
+                    cout<<"1"<<" ";
+                else
+                    cout<<"0"<<" ";
+                cout<<endl;
+            }
+        }
+    }
+    return 0;
+}

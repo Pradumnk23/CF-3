7731eb69feebbf665a07998feabc39030a891659
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-06-07 20:41:03 +0530

    check palindrome

---
+++ Antipalindrome.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define mod                 1000000007
+
+bool palindrome(string s)
+{
+    lli n=s.length();
+    lli l=0, h=n-1;
+    while(l<h)
+    {
+        if(s[l]!=s[h])
+            return false;
+        l++;
+        h--;
+    }
+    return true;
+}
+
+int main()
+{
+    fastio;
+        string s;
+        cin>>s;
+        set<char>st;
+        lli cnt=0;
+        for(lli i=0; i<s.length(); i++)
+        {
+            st.insert(s[i]);
+        }
+        if(!palindrome(s))
+            cout<<s.length()<<endl;
+
+        else if(palindrome(s))
+        {
+            if(st.size()==1)
+                cout<<0<<endl;
+
+            else
+                cout<<s.length()-1<<endl;
+        }
+    return 0;
+}
